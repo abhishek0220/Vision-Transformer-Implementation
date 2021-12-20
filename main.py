1d58978cbdb8e9753d601f4bf4d13e497ea3758c
abhishek0220 <abhishekchaudhary0220@gmail.com>
2021-12-20 11:59:34 +0530

    Attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from typing import Callable
 
 
 class PatchEmbed(nn.Module):
@@ -27,8 +28,8 @@ class PatchEmbed(nn.Module):
         self.patch_size = patch_size
         self.n_patches = (img_size // patch_size) ** 2
         self.proj = nn.Conv2d(  # Convolutional layer to split into patches and embed them
-            in_channels=in_channels,
-            out_channels=embed_dim,
+            in_channels,
+            embed_dim,
             kernel_size=patch_size,
             stride=patch_size,
         )
@@ -50,3 +51,73 @@ class PatchEmbed(nn.Module):
         x = x.transpose(1, 2)       # shape: (batch_size, n_patches, embed_dim)
         return x
 
+
+class Attention(nn.Module):
+    def __init__(self, dim: int, n_heads: int = 12, qkv_bias: bool = True, attn_p=0.0, proj_p=0.0):
+        """
+        Parameters
+        ----------
+        dim : int
+            Dimension of features per token
+        n_heads : int
+            Number of attention heads
+        qkv_bias : bool
+            Whether to add bias to the query, key, value
+        attn_p : float
+            Dropout probability for the attention
+        proj_p : float
+            Dropout probability for the projection
+        """
+        super().__init__()
+        self.dim = dim
+        self.n_heads = n_heads
+        self.head_dim = dim // n_heads
+        self.scale = 1/ (self.head_dim ** 0.5)
+        self.qkv: Callable[[torch.Tensor], torch.Tensor] = nn.Linear(dim, 3 * dim, bias=qkv_bias)
+        self.attn_drop: Callable[[torch.Tensor], torch.Tensor]  = nn.Dropout(attn_p)
+        self.proj: Callable[[torch.Tensor], torch.Tensor] = nn.Linear(dim, dim)
+        self.proj_drop: Callable[[torch.Tensor], torch.Tensor] = nn.Dropout(proj_p)
+
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input tensor of shape (batch_size, n_patches+1, dim)
+        
+        Returns
+        -------
+        torch.Tensor
+            Output tensor of shape (batch_size, n_patches+1, dim)
+        """
+        n_samples, n_tokens, dim = x.shape
+
+        if dim != self.dim:
+            raise ValueError
+
+        # Linear Tranformation
+        qkv= self.qkv(x)         # shape: (batch_size, n_patches+1, 3*dim)
+        qkv = qkv.reshape(
+            n_samples, n_tokens, 3, self.n_heads, self.head_dim
+        )                                       # shape: (batch_size, n_patches+1, 3, n_heads, head_dim)
+        qkv = qkv.permute(2, 0, 3, 1, 4)        # shape: (3, batch_size, n_heads, n_patches+1, head_dim)
+
+        q, k, v = qkv[0], qkv[1], qkv[2]        # shape: (batch_size, n_heads, n_patches+1, head_dim)
+        k_t = k.transpose(-2, -1)               # shape: (batch_size, n_heads, head_dim, n_patches+1)
+        dp = q.matmul(k_t) * self.scale         # shape: (batch_size, n_heads, head_dim, n_patches+1)
+        attn = dp.softmax(dim=-1)               # shape: (batch_size, n_heads, head_dim, n_patches+1)
+        attn = self.attn_drop(attn)             # shape: (batch_size, n_heads, head_dim, n_patches+1)
+
+        weighted_avg = attn.matmul(v)                # shape: (batch_size, n_heads, n_patches+1, head_dim)
+        weighted_avg = weighted_avg.transpose(1, 2)  # shape: (batch_size, n_patches+1, n_heads, head_dim)
+        weighted_avg = weighted_avg.flatten(2)       # shape: (batch_size, n_patches+1, dim)
+
+        x = self.proj(weighted_avg)             # shape: (batch_size, n_patches+1, dim)
+        x = self.proj_drop(x)                   # shape: (batch_size, n_patches+1, dim)
+
+        return x
+
+
+
+    
